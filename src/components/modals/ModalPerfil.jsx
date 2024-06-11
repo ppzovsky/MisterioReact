0b44810f1ec0e124e96716c8a539c168ab400d0a
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-06-10 21:41:57 -0300

    fix bugs in sidebar

--- src/components/modals/ModalPerfil.jsx
+++ src/components/modals/ModalPerfil.jsx
@@ -1,53 +1,69 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './modalPerfil.module.css';
 import PropTypes from 'prop-types';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const ModalPerfil = ({ isOpen, onClose }) => {
-
   const [perfil, setPerfil] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const dados = localStorage.getItem('login');
     if (dados) {
-        setPerfil(JSON.parse(dados));
+      setPerfil(JSON.parse(dados));
+    }
+    else{
+      setPerfil(null);
     }
   }, []);
 
-  if (!isOpen) return null; 
-
   return (
-    <motion.div
-    initial={{ opacity: 0, x: '-50vw', y: '-50vh' }} 
-    animate={{ opacity: 1, x: 0, y: 0 }} 
-    exit={{ opacity: 0, x: '50vw', y: '-50vh' }}
-    transition={{ ease: "easeOut", duration: 2 }}
-    style={{
-      position: 'absolute',
-      top: '50%',
-      left: '335%',
-      transform: 'translate(-50%, -50%)'
-    }}
-      >
-      <div className={styles.modalOverlay}>
-          <div className={styles.modalContent}>
-              <div className={styles.imageGrid}>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1}}
+            exit={{ opacity: 0}}
+            transition={{ ease: "easeOut", duration: 1 }}
+          >
+            <div className={styles.modalOverlay}>
+              <div className={styles.modalContent}>
+              {perfil ? (
+                <>
+                <div className={styles.imageGrid}>
                   <img
-                  src={perfil.imageSrc}
-                  alt='Foto Perfil'
-                  className={styles.imageOption}
+                    src={perfil.imageSrc}
+                    alt="Foto Perfil"
+                    className={styles.imageOption}
                   />
+                </div>
+                <h1 className={styles.titulo} id={styles.username}>
+                  {perfil.username}
+                </h1>
+                <h2 className={styles.titulo}>{perfil.email}</h2>
+                <button onClick={onClose} className={styles.botao}>
+                  Sair
+                </button>
+              </>
+                ) : (
+                  <>
+                  <h2 className={styles.titulo}>Você não fez login ainda</h2>
+                  <button onClick={() => navigate('/login')} className={styles.botaoLogin}>
+                    Login
+                  </button>
+                  <button onClick={onClose} className={styles.botao}>
+                    Sair
+                  </button>
+                </>
+                )}
               </div>
-              <h1 className={styles.titulo} id={styles.username}>{perfil.username}</h1>
-              <h2 className={styles.titulo}>{perfil.email}</h2>
-              <button onClick={() => {
-                      onClose();
-                  }} className={styles.botao}>Sair</button>
-          </div>
-      </div>
-    </motion.div>
-  );
-};
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    );
+  };
 
 ModalPerfil.propTypes = {
     isOpen: PropTypes.bool.isRequired,
